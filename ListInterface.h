d3ebe6e80c4293e9d11697e20cdf59a67ab7ea1c
scott-kirsch000 <kirsch000@live.missouristate.edu>
2019-03-20 13:04:18 -0500

    ListInterface.h init push

---
+++ ListInterface.h
@@ -0,0 +1,25 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University
+ * Spring 2019
+ *
+ * @file ListInterface.h
+ * @brief List prototype class template.
+ * @authors Scott Kirsch <kirsch000@live.missouristate.edu>
+ *
+ */
+
+#ifndef CSC232_LISTINTERFACE_
+#define CSC232_LISTINTERFACE_
+
+template<typename ItemType>
+class ListInterface
+{
+public:
+    virtual bool isEmpty() const =0;
+    virtual int getLength() const =0;
+    vir
+};
+
+
+#endif
