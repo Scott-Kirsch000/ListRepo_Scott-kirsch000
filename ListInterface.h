809a248aa6c50a0ce176ab4a94f91e41f8324f45
scott-kirsch000 <kirsch000@live.missouristate.edu>
2019-03-20 13:18:35 -0500

    Final ListInterface.h

--- ListInterface.h
+++ ListInterface.h
@@ -16,9 +16,29 @@ template<typename ItemType>
 class ListInterface
 {
 public:
+    /**
+     * ListInterface accessor to view if class object is empty.
+     *
+     * @pre None.
+     * @post The function returns a boolean value indicating if the class object is empty.
+     * @return bool  True if class object is empty, false if not.
+     */
     virtual bool isEmpty() const =0;
+
+    /**
+     * ListInterface accessor to view the length of the class object.
+     *
+     * @pre None.
+     * @post getLength return the number of items in the class object.
+     * @return int value representing the quantity of items in the class object.
+     */
     virtual int getLength() const =0;
-    vir
+
+
+    virtual bool insert(const int& itemPosition, const ItemType& newItem) =0;
+    virtual bool remote(const int& itemPostion) =0;
+    virtual ItemType getItem(const int& itemPostion) =0;
+    virtual ItemType replace(const int& itemPostion, const ItemType& newItem) =0;
 };
 
 
